d982c665d0965f3ac29b6bb2b31cc14c03eb2b11
benoitpas <bpasquereau@gmail.com>
2023-11-15 21:17:02 +0000

    Day1 solution

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -9,9 +9,18 @@ object MyApp extends ZIOAppDefault {
   def process = (buffer :BufferedSource)=> ZIO.attempt(buffer.getLines.toList)
   val readFile : Task[List[String]] = ZIO.acquireReleaseWith(acquire)(release)(process)
 
+  def calories(snacks:List[String]) =
+    val fold = snacks.foldRight((List[Int](),0))((str,state) => (str,state) match
+      case ("",(l,a)) => (a::l,0)
+      case (v,(l,a)) => (l, a+v.toInt)
+    )
+    fold._2::fold._1
+
   def run =
     for {
       v    <- readFile
-      _    <- printLine(v)
+      c = calories(v)
+      _    <- printLine(s"part1 = ${c.reduce(math.max)}")
+      _    <- printLine(s"part2 = ${c.sortBy((x) => -x).take(3).sum}")
     } yield ()
 }
